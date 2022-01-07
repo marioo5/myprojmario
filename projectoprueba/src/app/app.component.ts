f564a2e17b18a04664abc59d7bfb7fa940186417
mario.carreras <mario.carreras@bosonit.com>
2022-01-07 19:12:29 +0100

    ENTREGA 3. ANGULAR. DIRECTIVAS.DIRECTIVAS COMPLETO

--- projectoprueba/src/app/app.component.ts
+++ projectoprueba/src/app/app.component.ts
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectoprueba';
-  // condicion:boolean = true;
+  condicion:boolean = true;
+  verAutor: any;
+  libros! : Array<any>;
+  vblecolor!: string;
+  color!: string;
+  userObservable!: boolean;
+
+
+constructor () {
+  this.libros =[
+    {id:'1',titulo:'La bestia', autor:'Carmen Mola'},
+    {id:'2',titulo:'Últimos días en Berlín', autor:'Paloma'},
+    {id:'3',titulo:'El principe persia', autor:'Fernando'},
+  ];
+
+}
+
+showAuthor(_libro:any){
+  this.verAutor= 'Escrito por' + _libro.autor;
+  alert(this.verAutor);
+}
 
 }
